6bfb53bc52ee86bbdea5bd695a9b60ab485d2241
Matias Alves <matias.alves2002@gmail.com>
2024-05-22 20:26:36 -0300

    HashPredecir

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     private static final int REPETICIONES = 100;
 
-    public static void main(String[] args){
-        TArbolTrie trie = new TArbolTrie();      
+    public static void main(String[] args) {
+        TArbolTrie arbolTrie = new TArbolTrie();
         LinkedList linkedList = new LinkedList();
         ArrayList arrayList = new ArrayList();
         HashMap hashMap = new HashMap();
@@ -13,36 +13,52 @@ public class Main {
         String[] palabrasclave = ManejadorArchivosGenerico.leerArchivo("./src/listado-general_desordenado.txt");
         String[] palabrasBuscar = ManejadorArchivosGenerico.leerArchivo("./src/listado-general_palabrasBuscar.txt");
         for (String p : palabrasclave) {
-                // insertar la palabra p en el trie
-                // insertar la palabra p en el linkedList
-                // insertar la palabra p en el arrayList
-                // insertar la palabra p en el hashMap
-                // insertar la palabra p en el treeMap
-            trie.insertar(p);
+            // insertar la palabra p en el trie
+            // insertar la palabra p en el linkedList
+            // insertar la palabra p en el arrayList
+            // insertar la palabra p en el hashMap
+            // insertar la palabra p en el treeMap
+            arbolTrie.insertar(p);
             linkedList.add(p);
             arrayList.add(p);
             hashMap.put(p, p);
-            treeMap.put(p,p);
+            treeMap.put(p, p);
         }
-        Medible[] medibles = new Medible[5];
+        Medible[] medibles = new Medible[6];
         int i = 0;
+
         medibles[i++] = new MedicionBuscarLinkedList(linkedList);
         medibles[i++] = new MedicionBuscarArrayList(arrayList);
-        medibles[i++] = new MedicionBuscarTrie(trie);
+        medibles[i++] = new MedicionBuscarTrie(arbolTrie);
         medibles[i++] = new MedicionBuscarHashMap(hashMap);
         medibles[i++] = new MedicionBuscarTreeMap(treeMap);
+
+        medibles[i++] = new MedicionPredecirHashMap(hashMap);
+
         Medicion mi;
-	i = 0;
-        Object[] params = {REPETICIONES, palabrasBuscar};
-        String[] lineas = new String[6];
-		lineas[i++] = "algoritmo,tiempo,memoria";
-		for (Medible m: medibles){
-            mi= m.medir(params);
-			mi.print();
-			lineas[i++] = mi.getTexto()+","+mi.getTiempoEjecucion().toString()+","+mi.getMemoria().toString();
-			
+        i = 0;
+        // String prefijo = "cas";
+        Object[] params = { REPETICIONES, palabrasBuscar };
+        String[] lineas = new String[7];
+        lineas[i++] = "algoritmo,tiempo,memoria";
+        for (Medible m : medibles) {
+            mi = m.medir(params);
+            mi.print();
+            lineas[i++] = mi.getTexto() + "," + mi.getTiempoEjecucion().toString() + ","
+                    + mi.getMemoria().toString();
+
         }
-        
-		 ManejadorArchivosGenerico.escribirArchivo("./src/salida.csv",lineas);
+
+        ManejadorArchivosGenerico.escribirArchivo("./src/salida.csv", lineas);
+
+        // arbolTrie.insertar("prediccion");
+        // arbolTrie.insertar("predecir");
+        // arbolTrie.insertar("presente");
+        // arbolTrie.insertar("presionar");
+
+        // LinkedList<String> predicciones = arbolTrie.predecir("pre");
+        // for (String palabra : predicciones) {
+        // System.out.println(palabra);
+        // }
     }
 }
--- src/MedicionBuscarHashMap.java
+++ src/MedicionBuscarHashMap.java
@@ -1,17 +1,18 @@
 import java.util.HashMap;
 
-public class MedicionBuscarHashMap extends Medible{
+public class MedicionBuscarHashMap extends Medible {
     private HashMap hashmap;
 
     public MedicionBuscarHashMap(HashMap hashMap) {
         this.hashmap = hashMap;
     }
+
     @Override
     public void ejecutar(Object... params) {
         int repeticion = (int) params[0];
         String[] palabras = (String[]) params[1];
-        for(int i = 0; i < repeticion; i++){
-            for(String palabra : palabras){
+        for (int i = 0; i < repeticion; i++) {
+            for (String palabra : palabras) {
                 hashmap.containsKey(palabra);
             }
         }
